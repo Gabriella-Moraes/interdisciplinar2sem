18ba44ff96cb126f29d43a2005b3949b55f8d13b
Gabriella-Moraes <gabriellamoraes58@gmail.com>
2022-06-09 00:23:50 -0300

    update mysql

--- controller/SQL/db.js
+++ controller/SQL/db.js
@@ -6,12 +6,8 @@ async function connect(){
 
     const mysql = require("mysql2/promise");
     //const connection = await mysql.createConnection("mysql://root:alunofatec@localhost:3306/interSecretaria");
-<<<<<<< HEAD
     const connection = await mysql.createConnection("mysql://root:teste1@localhost:3306/interSecretaria");
     //const connection = await mysql.createConnection("mysql://root:Carol120599@localhost:3306/interSecretaria");
-=======
-    const connection = await mysql.createConnection("mysql://root:Carol120599@localhost:3306/interSecretaria");
->>>>>>> 7977c03bd50ef7d6d7e6f2d9fe9bc400675ea121
     console.log("conectou no MySQL");
     global.connection = connection;
     return connection;
